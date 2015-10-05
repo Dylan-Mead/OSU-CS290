8badbc18a150a5f87aa00163043d0ed68c162347
Dylan-Mead <dylanmead@dylans-MacBook-Air.local>
2015-10-04 18:36:39 -0700

    first java program
    This is really just another text file so we’re not learning anything
    new by submitting this. :S

---
+++ RealProgram.java
@@ -0,0 +1,16 @@
+
+
+class RealProgram{
+
+   public static void main(String[] args){
+
+      System.out.println("I am a real program that really does something!");
+      System.out.println("Watch me math.");
+      int a = 1;
+      int b = 2;
+      System.out.println("The sum of "+ a +" and "+ b +" is "+ (a+b));
+      System.out.println("Impressed?");
+
+   }
+
+}
